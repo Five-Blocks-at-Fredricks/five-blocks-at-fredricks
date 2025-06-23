d7cfd3f83a4f69c8e5519f4ada4940922ac7e0bd
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-06-23 10:48:21 -0700

    Initial commit

---
+++ Scripts/CameraRotate.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class CameraRotate : Camera3D {
+    public override void _Process(double delta) {
+        Vector2 MousePos = GetViewport().GetMousePosition();
+        float ScreenWidth = GetViewport().GetVisibleRect().Size.X;
+
+        if (MousePos.X > ScreenWidth * 0.75 && RotationDegrees.Y > -42.238636) {
+            RotationDegrees = new Vector3(RotationDegrees.X, RotationDegrees.Y - 90f * (float)delta, RotationDegrees.Z);
+        }
+
+        if (MousePos.X < ScreenWidth * 0.25 && RotationDegrees.Y < 42.238636) {
+            RotationDegrees = new Vector3(RotationDegrees.X, RotationDegrees.Y + 90f * (float)delta, RotationDegrees.Z);
+        }
+    }
+}
