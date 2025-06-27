50b2923042df7984307a8fdaf4f17dbb4e04f20e
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-06-26 18:19:04 -0700

    Made the flashlight flicker
    This was really annoying but it works now yayayayayayya

---
+++ Scripts/LightFlicker.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+// This script was mostly made by chatgpt since godot was being a lil special
+public partial class LightFlicker : Node3D {
+    [Export] public float MinFlickerSpeed = 0.05f;
+    [Export] public float MaxFlickerSpeed = 0.1f;
+
+    [Export] public Godot.Collections.Array<NodePath> LightPaths = new();
+    [Export] public Godot.Collections.Array<float> MinEnergies = new();
+    [Export] public Godot.Collections.Array<float> MaxEnergies = new();
+
+    private class LightData {
+        public Light3D Light;
+        public float MinEnergy;
+        public float MaxEnergy;
+        public bool IsMin = true;
+    }
+
+    private List<LightData> lights = new List<LightData>();
+    private float timer = 0f;
+    private float flickerSpeed;
+    private Random random = new Random();
+
+    public override void _Ready() {
+        int count = Mathf.Min(LightPaths.Count, Mathf.Min(MinEnergies.Count, MaxEnergies.Count));
+
+        for (int i = 0; i < count; i++) {
+            Light3D light = GetNodeOrNull<Light3D>(LightPaths[i]);
+            if (light != null) {
+                lights.Add(new LightData {
+                    Light = light,
+                    MinEnergy = MinEnergies[i],
+                    MaxEnergy = MaxEnergies[i],
+                });
+            }
+        }
+
+        flickerSpeed = GetNextFlickerSpeed();
+    }
+
+    public override void _Process(double delta) {
+        timer += (float)delta;
+
+        if (timer >= flickerSpeed) {
+            timer = 0f;
+            flickerSpeed = GetNextFlickerSpeed();
+
+            foreach (var light in lights) {
+                light.IsMin = !light.IsMin;
+                light.Light.LightEnergy = light.IsMin ? light.MinEnergy : light.MaxEnergy;
+            }
+        }
+    }
+
+    private float GetNextFlickerSpeed() {
+        return (float)(MinFlickerSpeed + random.NextDouble() * (MaxFlickerSpeed - MinFlickerSpeed));
+    }
+}
