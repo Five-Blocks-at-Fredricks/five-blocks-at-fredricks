ff1bc61ca405d5b4c3a33a34b278529c21828a29
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-07-11 12:09:36 -0700

    Added an hour system
    Nothing actually happens when you reach 6 am yet but lncvrt says he will
    make that.

--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -5,6 +5,7 @@ public partial class Globals : Node {
     public static int ActiveCamera = 0;
     public static List<Camera3D> Cameras = [];
     public static bool MovableCamera = false;
+    public static int Time = 0;
 
     public static void ResetGlobals() {
         ActiveCamera = 0;
---
+++ Scripts/TimeLabelUpdater.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class TimeLabelUpdater : Label {
+    private string DisplayedTime = "12 AM";
+    private float TimeTimer = 0f;
+
+    public override void _Process(double delta) {
+        if (TimeTimer < 0.9f) {
+            TimeTimer += (float)delta;
+        } else {
+            TimeTimer = 0f;
+        }
+
+        if (TimeTimer <= 1.1f && TimeTimer >= 0.9f) {
+            Globals.Time += 1;
+        }
+
+        if (Globals.Time == 60) {
+            DisplayedTime = "1 AM";
+        } else if (Globals.Time == 120) {
+            DisplayedTime = "2 AM";
+        } else if (Globals.Time == 180) {
+            DisplayedTime = "3 AM";
+        } else if (Globals.Time == 240) {
+            DisplayedTime = "4 AM";
+        } else if (Globals.Time == 300) {
+            DisplayedTime = "5 AM";
+        } else if (Globals.Time == 360) {
+            DisplayedTime = "6 AM";
+        }
+
+        Text = DisplayedTime;
+    }
+}
