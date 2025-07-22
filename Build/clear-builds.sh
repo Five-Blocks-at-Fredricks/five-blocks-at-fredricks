53fafca1ec7c01a08827c356c043cffd53d25991
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-07-22 14:49:44 -0700

    Added a zip builds script
    The clear builds script also deletes the zip files

--- Build/clear-builds.sh
+++ Build/clear-builds.sh
@@ -7,3 +7,8 @@ touch Build/linux/.gitkeep
 touch Build/windows/.gitkeep
 touch Build/mac/.gitkeep
 touch Build/linux-arm/.gitkeep
+
+rm -rf Build/fbaf-linux-arm.zip
+rm -rf Build/fbaf-linux.zip
+rm -rf Build/fbaf-windows.zip
+rm -rf Build/fbaf-mac.zip
---
+++ Build/zip-builds.sh
@@ -0,0 +1,4 @@
+7z a Build/fbaf-linux.zip "Build/linux/data_Five Blocks at Fredricks_linuxbsd_x86_64" Build/linux/fbaf.pck Build/linux/fbaf.x86_64
+7z a Build/fbaf-linux-arm.zip "Build/linux-arm/data_Five Blocks at Fredricks_linuxbsd_arm64" Build/linux-arm/fbaf.pck Build/linux-arm/fbaf.arm64
+7z a Build/fbaf-windows.zip "Build/windows/data_Five Blocks at Fredricks_windows_x86_64" Build/windows/fbaf.pck Build/windows/fbaf.exe
+7z a Build/fbaf-mac.zip Build/mac/fbaf.app
