6968dfb78e7bd7656e0a2d918e75ea1c81a20160
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-07-22 15:34:55 -0700

    Fixed the zip builds script including parent folders in the zips
    I also released a beta build!

--- Build/zip-builds.sh
+++ Build/zip-builds.sh
@@ -1,4 +1,4 @@
-7z a Build/fbaf-linux.zip "Build/linux/data_Five Blocks at Fredricks_linuxbsd_x86_64" Build/linux/fbaf.pck Build/linux/fbaf.x86_64
-7z a Build/fbaf-linux-arm.zip "Build/linux-arm/data_Five Blocks at Fredricks_linuxbsd_arm64" Build/linux-arm/fbaf.pck Build/linux-arm/fbaf.arm64
-7z a Build/fbaf-windows.zip "Build/windows/data_Five Blocks at Fredricks_windows_x86_64" Build/windows/fbaf.pck Build/windows/fbaf.exe
-7z a Build/fbaf-mac.zip Build/mac/fbaf.app
+(cd Build/linux && 7z a ../../Build/fbaf-linux.zip "data_Five Blocks at Fredricks_linuxbsd_x86_64" fbaf.pck fbaf.x86_64)
+(cd Build/linux-arm && 7z a ../../Build/fbaf-linux-arm.zip "data_Five Blocks at Fredricks_linuxbsd_arm64" fbaf.pck fbaf.arm64)
+(cd Build/windows && 7z a ../../Build/fbaf-windows.zip "data_Five Blocks at Fredricks_windows_x86_64" fbaf.pck fbaf.exe)
+(cd Build/mac && 7z a ../../Build/fbaf-mac.zip fbaf.app)
