223d0b747add043892619fcfcefbf5464d2a17fd
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-07-21 22:20:56 -0700

    Added a script that clears the builds
    idk if this is the easiest way to make it so you can easily clear builds
    but idc

---
+++ Build/clear-builds.sh
@@ -0,0 +1,9 @@
+rm -rf Build/linux/*
+rm -rf Build/windows/*
+rm -rf Build/mac/*
+rm -rf Build/linux-arm/*
+
+touch Build/linux/.gitkeep
+touch Build/windows/.gitkeep
+touch Build/mac/.gitkeep
+touch Build/linux-arm/.gitkeep
