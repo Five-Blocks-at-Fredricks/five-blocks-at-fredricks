190df81a3699460b4cbd8fafaafb4784178d6cd4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-07-29 19:08:49 -0700

    Fixed it trying to load dialog in custom night
    I don't plan on custom night having dialog

--- Scripts/DialogPlayer.cs
+++ Scripts/DialogPlayer.cs
@@ -4,10 +4,12 @@ using System;
 public partial class DialogPlayer : AudioStreamPlayer3D {
     private AudioStreamMP3 DialogMessage = new();
     public override void _Ready() {
-        DialogMessage = GD.Load<AudioStreamMP3>($"res://Audio/Dialog/Night{Globals.Night}.mp3");
+        if (Globals.Night != 6) {
+            DialogMessage = GD.Load<AudioStreamMP3>($"res://Audio/Dialog/Night{Globals.Night}.mp3");
 
-        Stream = DialogMessage;
+            Stream = DialogMessage;
 
-        Play();
+            Play();
+        }
     }
 }
